05d77966c0743b6627d47ca3fbb18fd78b4c620e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-02 22:24:40 +0800

    Draft logo animation for splash screen

---
+++ app/src/main/java/com/mobdeve/s14/gonzales/mark/artemis/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.mobdeve.s14.gonzales.mark.artemis
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    private var logoAnimation: AnimationDrawable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ivLogo = findViewById<ImageView>(R.id.iv_logo)
+        ivLogo.setBackgroundResource(R.drawable.logo_animation)
+        logoAnimation = ivLogo.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        logoAnimation!!.setEnterFadeDuration(300);
+        logoAnimation!!.setExitFadeDuration(300);
+        logoAnimation!!.start()
+    }
+}
