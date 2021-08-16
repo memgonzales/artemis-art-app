17f5b5bac6b6a71972698937d06f7b45140dc047
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-17 01:01:05 +0800

    Connect splash to partially completed log-in

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/MainActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private var logoAnimation: AnimationDrawable? = null
+
+    companion object {
+        private const val TIME_OUT = 3000
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         val ivLogo = findViewById<ImageView>(R.id.iv_logo)
         ivLogo.setBackgroundResource(R.drawable.logo_animation)
         logoAnimation = ivLogo.background as AnimationDrawable
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this@MainActivity, LogInActivity::class.java)
+            startActivity(i)
+            finish()
+        }, TIME_OUT.toLong())
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
