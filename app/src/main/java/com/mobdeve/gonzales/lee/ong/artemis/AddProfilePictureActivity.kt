94fed773e80368152501542ce0e8651290332a2d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-18 03:06:41 +0800

    Layout top portion of add profile picture

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfilePictureActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfilePictureActivity.kt
@@ -1,11 +1,12 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddPicture : AppCompatActivity() {
+class AddProfilePictureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_picture)
+        setContentView(R.layout.activity_add_profile_picture)
     }
 }
--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/SignUpActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/SignUpActivity.kt
@@ -1,9 +1,13 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
+    private var btnSignUp: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -12,12 +16,23 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun initComponents() {
-        setSupportActionBar(findViewById(R.id.toolbar_sign_up))
+        setSupportActionBar(findViewById(R.id.toolbar_profile))
         initActionBar()
+
+        this.btnSignUp = findViewById(R.id.btn_sign_up)
+        launchAddProfilePic()
     }
 
     private fun initActionBar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun launchAddProfilePic() {
+        this.btnSignUp?.setOnClickListener {
+            val i = Intent(this@SignUpActivity, AddProfilePictureActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
