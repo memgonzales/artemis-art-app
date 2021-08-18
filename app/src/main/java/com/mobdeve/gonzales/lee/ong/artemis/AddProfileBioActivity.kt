f3693cb7aa3db3bc328457dc0a46d247b5d50c4b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-18 14:52:05 +0800

    Tweak scaling for header subtitle overflow

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfileBioActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfileBioActivity.kt
@@ -1,11 +1,38 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddProfileBioActivity : AppCompatActivity() {
+    private var btnAddBio: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_profile_bio)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar_add_profile_bio))
+        initActionBar()
+
+        this.btnAddBio = findViewById(R.id.btn_add_profile_bio_add)
+        launchAddProfilePic()
+    }
+
+    private fun initActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun launchAddProfilePic() {
+        this.btnAddBio?.setOnClickListener {
+            val i = Intent(this@AddProfileBioActivity, AddProfilePictureActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
