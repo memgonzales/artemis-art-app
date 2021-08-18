0996247ba2afaef4fe798ecdf401cd786e52d294
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-18 10:41:50 +0800

    Tweak logo-text padding

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfilePictureActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfilePictureActivity.kt
@@ -3,10 +3,29 @@ package com.mobdeve.gonzales.lee.ong.artemis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddProfilePictureActivity : AppCompatActivity() {
+    private var btnAddProfilePic: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_profile_picture)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar_profile))
+
+        this.btnAddProfilePic = findViewById(R.id.btn_add_profile_pic_add)
+        launchAddBio()
+    }
+
+    private fun launchAddBio() {
+        this.btnAddProfilePic?.setOnClickListener {
+            val i = Intent(this@AddProfilePictureActivity, SignUpActivity::class.java)
+            startActivity(i)
+        }
     }
 }
