e559f48eca16ee4061cfd7e83aac3aab3fde1149
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-18 10:16:21 +0800

    Finish add profile picture layout

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AddProfileBioActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProfileBioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_profile_bio)
+    }
+}
