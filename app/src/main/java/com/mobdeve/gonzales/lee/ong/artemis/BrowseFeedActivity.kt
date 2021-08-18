ed0c67bd0a8e3fc0847beaea68a0913271fc9d8d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-19 01:59:08 +0800

    Add bottom navigation bar and ripple effect

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrowseFeedActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrowseFeedActivity.kt
@@ -2,10 +2,26 @@ package com.mobdeve.gonzales.lee.ong.artemis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ActionMenuView
+import androidx.appcompat.widget.Toolbar
 
 class BrowseFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse_feed)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar_feed))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_feed, menu)
+
+        return true
     }
 }
