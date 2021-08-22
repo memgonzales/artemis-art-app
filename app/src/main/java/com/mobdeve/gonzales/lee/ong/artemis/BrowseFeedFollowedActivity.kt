e42102ec59174d6c9dc4f808ee093365eaebb079
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-23 01:32:44 +0800

    Finish layout for view feed of followed users

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrowseFeedFollowedActivity.kt
@@ -0,0 +1,65 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
+
+class BrowseFeedFollowedActivity : AppCompatActivity() {
+    private lateinit var dataPosts: ArrayList<Post>
+    private lateinit var rvFeed: RecyclerView
+    private lateinit var feedAdapter: FeedAdapter
+    private lateinit var sflFeed: ShimmerFrameLayout
+
+    companion object {
+        private const val SHIMMER_TIMEOUT = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse_feed_followed)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar_feed_followed))
+        initShimmer()
+    }
+
+    private fun initShimmer() {
+        this.sflFeed = findViewById(R.id.sfl_feed)
+
+        sflFeed.startShimmer()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            initRecyclerView()
+            sflFeed.visibility = View.GONE
+            rvFeed.visibility = View.VISIBLE
+        }, SHIMMER_TIMEOUT.toLong())
+    }
+
+    private fun initRecyclerView() {
+        this.dataPosts = DataHelper.loadPostData();
+
+        this.rvFeed = findViewById(R.id.rv_feed);
+        this.rvFeed.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        this.feedAdapter = FeedAdapter(this.dataPosts);
+
+
+        this.rvFeed.adapter = feedAdapter;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_feed, menu)
+
+        return true
+    }
+}
