8ddeb6ca3c31ea981e520de76f537d16446f3550
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-22 23:27:26 +0800

    Make picture width match parent

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/SearchResultsActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchResultsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_results)
+    }
+}
