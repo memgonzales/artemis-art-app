555dddee6ce2aa7da041e03d1db10b718ab67131
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-22 21:48:54 +0800

    Revise dummy data

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/DataHelper.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/DataHelper.kt
@@ -22,73 +22,73 @@ object DataHelper {
                 1080,
                 "Its form is a cuckoo. The master of this emotion is aware of how pitiful her Doppel is, and refuses to look at it. This Doppel wordlessly wraps around and strangles anything it doesn't want to hear as it continues its search for something to fill the hole in its heart.",
                 arrayTags,
-                false
+                true
             )
         )
         data.add(
             Post(
-                R.drawable.shoob,
-                "shooberino",
                 R.drawable.shibe1,
-                "The Doppel of Silence and Iroha Tamaki from Magia Record",
-                100,
-                50,
+                "shibe",
+                R.drawable.gorgeous_irene,
+                "Gorgeous Irene",
+                90,
+                10,
                 CustomDate(2021, 7, 20),
                 "Digital Art",
-                1080,
-                1080,
-                "Its form is a cuckoo. The master of this emotion is aware of how pitiful her Doppel is, and refuses to look at it. This Doppel wordlessly wraps around and strangles anything it doesn't want to hear as it continues its search for something to fill the hole in its heart.",
+                900,
+                900,
+                "Irene is a professional assassin who is able to transform into any type of woman using her 'make-up' power, but instead of killing in cold blood, she uses her abilities for good.",
                 arrayTags,
                 false
             )
         )
         data.add(
             Post(
-                R.drawable.shoob,
-                "shooberino",
-                R.drawable.shibe4,
-                "The Doppel of Silence and Iroha Tamaki from Magia Record",
-                100,
-                50,
-                CustomDate(2021, 7, 20),
-                "Digital Art",
-                1080,
-                1080,
-                "Its form is a cuckoo. The master of this emotion is aware of how pitiful her Doppel is, and refuses to look at it. This Doppel wordlessly wraps around and strangles anything it doesn't want to hear as it continues its search for something to fill the hole in its heart.",
-                arrayTags,
-                false
-            )
-        )
-        data.add(
-            Post(
-                R.drawable.shoob,
-                "shooberino",
                 R.drawable.shibe2,
-                "The Doppel of Silence and Iroha Tamaki from Magia Record",
-                100,
-                50,
+                "shoobie",
+                R.drawable.faraway_jobin,
+                "Faraway Jobin",
+                0,
+                10,
                 CustomDate(2021, 7, 20),
                 "Digital Art",
                 1080,
                 1080,
-                "Its form is a cuckoo. The master of this emotion is aware of how pitiful her Doppel is, and refuses to look at it. This Doppel wordlessly wraps around and strangles anything it doesn't want to hear as it continues its search for something to fill the hole in its heart.",
+                "Jobin is the eccentric eldest son of the Higashikata Family. He is married with Mitsuba Higashikata and has a son, Tsurugi.",
+                arrayTags,
+                true
+            )
+        )
+        data.add(
+            Post(
+                R.drawable.shibe3,
+                "shib",
+                R.drawable.surprised_gappy,
+                "Early Josuk8",
+                69,
+                42,
+                CustomDate(2021, 7, 20),
+                "Digital Art",
+                800,
+                800,
+                "Josuke is a young man afflicted with retrograde amnesia, lacking any memories prior to being discovered by Yasuho Hirose near the Wall Eyes in Morioh Town. He dedicates himself to discovering his former identity and those originally associated with him.",
                 arrayTags,
                 false
             )
         )
         data.add(
             Post(
-                R.drawable.shoob,
-                "shooberino",
-                R.drawable.shibe3,
-                "The Doppel of Silence and Iroha Tamaki from Magia Record",
-                100,
-                50,
+                R.drawable.shibe4,
+                "mameshiba",
+                R.drawable.so_key_visual,
+                "Stone Ocean Key Visual",
+                1000,
+                1357,
                 CustomDate(2021, 7, 20),
                 "Digital Art",
-                1080,
-                1080,
-                "Its form is a cuckoo. The master of this emotion is aware of how pitiful her Doppel is, and refuses to look at it. This Doppel wordlessly wraps around and strangles anything it doesn't want to hear as it continues its search for something to fill the hole in its heart.",
+                678,
+                1276,
+                "Set in 2011, Florida, Jolyne Cujoh, daughter of Jotaro, is wrongfully accused of a crime she didn't commit and sent to a maximum security prison. While imprisoned, she struggles within a longstanding plot agreed between dead villain DIO and ideologue Enrico Pucci.",
                 arrayTags,
                 true
             )
