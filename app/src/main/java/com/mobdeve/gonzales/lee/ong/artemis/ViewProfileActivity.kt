569c7f2886f12b310a5150c090a7abd37e2f7603
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-23 22:36:43 +0800

    Add search bar to view profile

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/ViewProfileActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/ViewProfileActivity.kt
@@ -7,5 +7,17 @@ class ViewProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_profile)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar_view_profile))
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 }
