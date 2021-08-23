cb21631aea25a438e1dedf76bd033ef964edfa69
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-23 12:06:35 +0800

    Fix save button above soft keyboard but hide bottom navbar

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
@@ -107,7 +107,7 @@ class LogInActivity : AppCompatActivity() {
 
     private fun startTesting() {
         this.btnTest.setOnClickListener {
-            val i = Intent(this@LogInActivity, BrowseFeedActivity::class.java)
+            val i = Intent(this@LogInActivity, EditCommentActivity::class.java)
             startActivity(i)
         }
     }
