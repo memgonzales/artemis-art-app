4f81a61335266c46009ecabeaaf2b2d66cce52ec
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-08-25 02:10:26 +0800

    Implement edit profile and account management

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AccountManagementActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/AccountManagementActivity.kt
@@ -1,19 +1,72 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ScrollView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AccountManagementActivity : AppCompatActivity() {
+    private lateinit var clAccountManagementDelete: ConstraintLayout
+    private lateinit var bnvAccountManagementBottom: BottomNavigationView
+    private lateinit var nsvAccountManagement: NestedScrollView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_management)
 
+        initContent()
         initComponents()
+    }
+
+    private fun initContent() {
+        this.clAccountManagementDelete = findViewById(R.id.cl_account_management_delete)
+        this.bnvAccountManagementBottom = findViewById(R.id.nv_account_management_bottom)
+
+        clAccountManagementDelete.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@AccountManagementActivity, "Account deleted.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@AccountManagementActivity, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
     }
 
     private fun initComponents() {
         setSupportActionBar(findViewById(R.id.toolbar_account_management))
+        initBottom()
         initActionBar()
+    }
+
+    private fun initBottom() {
+        bnvAccountManagementBottom.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.icon_home_profile -> {
+                    val intent = Intent(this@AccountManagementActivity, BrowseFeedActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.icon_follow_profile -> {
+                    val intent = Intent(this@AccountManagementActivity, BrowseFeedFollowedActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.icon_bookmark_profile -> {
+                    val intent = Intent(this@AccountManagementActivity, BrowseBookmarksActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.icon_user_profile -> {
+                    val intent = Intent(this@AccountManagementActivity, ViewProfileActivity::class.java)
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 
     private fun initActionBar() {
--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/EditProfileActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/EditProfileActivity.kt
@@ -1,14 +1,22 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class EditProfileActivity : AppCompatActivity() {
     private lateinit var fabEditProfilePicEdit: FloatingActionButton
     private lateinit var btmProfilePicture: BottomSheetDialog
+    private lateinit var clDialogProfilePictureEdit: ConstraintLayout
+    private lateinit var clDialogProfilePictureDelete: ConstraintLayout
+    private lateinit var btnEditProfileSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,15 @@ class EditProfileActivity : AppCompatActivity() {
 
         this.fabEditProfilePicEdit = findViewById(R.id.fab_edit_profile_pic_edit)
         this.btmProfilePicture = BottomSheetDialog(this@EditProfileActivity)
+        this.btnEditProfileSave = findViewById(R.id.btn_edit_profile_save)
+
+        btnEditProfileSave.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@EditProfileActivity, "Profile changes are saved.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@EditProfileActivity, ViewProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
         launchDialog()
     }
 
@@ -31,6 +48,20 @@ class EditProfileActivity : AppCompatActivity() {
 
         this.fabEditProfilePicEdit.setOnClickListener {
             btmProfilePicture.setContentView(view)
+
+            this.clDialogProfilePictureEdit = btmProfilePicture.findViewById(R.id.cl_dialog_profile_picture_edit)!!
+            this.clDialogProfilePictureDelete = btmProfilePicture.findViewById(R.id.cl_dialog_profile_picture_delete)!!
+
+            clDialogProfilePictureEdit.setOnClickListener(View.OnClickListener {
+                Toast.makeText(this@EditProfileActivity, "Profile picture is edited.", Toast.LENGTH_SHORT).show()
+                btmProfilePicture.dismiss()
+            })
+
+            clDialogProfilePictureDelete.setOnClickListener(View.OnClickListener {
+                Toast.makeText(this@EditProfileActivity, "Profile picture is deleted.", Toast.LENGTH_SHORT).show()
+                btmProfilePicture.dismiss()
+            })
+
             btmProfilePicture.show()
         }
     }
