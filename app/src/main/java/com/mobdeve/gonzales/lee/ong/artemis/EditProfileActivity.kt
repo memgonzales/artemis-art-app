916e6136af05db2f6ac46f2d6f664b6d1c6dbdd9
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-24 11:40:49 +0800

    Add bottom sheet dialog to edit profile (profile picture)

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/EditProfileActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/EditProfileActivity.kt
@@ -2,8 +2,14 @@ package com.mobdeve.gonzales.lee.ong.artemis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var fabEditProfilePicEdit: FloatingActionButton
+    private lateinit var btmProfilePicture: BottomSheetDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
@@ -14,6 +20,19 @@ class EditProfileActivity : AppCompatActivity() {
     private fun initComponents() {
         setSupportActionBar(findViewById(R.id.toolbar_edit_profile))
         initActionBar()
+
+        this.fabEditProfilePicEdit = findViewById(R.id.fab_edit_profile_pic_edit)
+        this.btmProfilePicture = BottomSheetDialog(this@EditProfileActivity)
+        launchDialog()
+    }
+
+    private fun launchDialog() {
+        val view = LayoutInflater.from(this@EditProfileActivity).inflate(R.layout.dialog_profile_picture, null)
+
+        this.fabEditProfilePicEdit.setOnClickListener {
+            btmProfilePicture.setContentView(view)
+            btmProfilePicture.show()
+        }
     }
 
     private fun initActionBar() {
