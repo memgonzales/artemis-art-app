c513f9b6d830be9bd37c7bba7d1fc85b2d0bc0ba
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-24 08:16:34 +0800

    Create skeletal files for post artwork activity

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/PostArtworkActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostArtworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_artwork)
+    }
+}
