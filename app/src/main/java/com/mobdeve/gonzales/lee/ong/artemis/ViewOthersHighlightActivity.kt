db9f28eb5f1c9c4a1446891fe261156065f8b744
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-24 20:41:23 +0800

    Rearrange text fields in post artwork activity

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/ViewOthersHighlightActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewOthersHighlightActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_others_highlight)
+    }
+}
