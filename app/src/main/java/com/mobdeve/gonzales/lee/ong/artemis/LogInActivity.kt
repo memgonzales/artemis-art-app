1c08a8c5e83019b7ab0b8b14ba7d33f868d542d7
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-08-26 22:23:53 +0800

    Add placeholder when there are no search results (unregistered)

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
@@ -206,7 +206,7 @@ class LogInActivity : AppCompatActivity() {
 
     private fun startTesting() {
         this.btnTest.setOnClickListener {
-            val i = Intent(this@LogInActivity, SearchResultsActivity::class.java)
+            val i = Intent(this@LogInActivity, SearchResultsUnregisteredActivity::class.java)
             startActivity(i)
         }
     }
