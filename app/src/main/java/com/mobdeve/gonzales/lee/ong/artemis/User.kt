3bab4d1f59d052d78335882d1fd4b1e72f3693c1
pbong <phoebs_ong@dlsu.edu.ph>
2021-08-31 20:31:09 +0800

    Fix Signup Email

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/User.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/User.kt
@@ -1,18 +1,19 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
 class User {
+    private lateinit var userId: String
     private var username: String
     private var email: String
     private var password: String
     private var userImg: Int
     private var bio: String
 
-    private var bookmarks: ArrayList<String>
-    private var userPosts: ArrayList<String>
-    private var highlights: ArrayList<String>
-    private var upvotedPosts: ArrayList<String>
-    private var usersFollowed: ArrayList<String>
-    private var userComments: ArrayList<String>
+    private lateinit var bookmarks: ArrayList<String>
+    private lateinit var userPosts: ArrayList<String>
+    private lateinit var highlights: ArrayList<String>
+    private lateinit var upvotedPosts: ArrayList<String>
+    private lateinit var usersFollowed: ArrayList<String>
+    private lateinit var userComments: ArrayList<String>
 
     constructor(username: String, email: String, password: String){
         this.username = username
@@ -21,12 +22,15 @@ class User {
         this.userImg = R.drawable.chibi_artemis_hd
         this.bio = ""
 
+        /*
         this.bookmarks = ArrayList<String>()
         this.userPosts = ArrayList<String>()
         this.highlights = ArrayList<String>()
         this.upvotedPosts = ArrayList<String>()
         this.usersFollowed = ArrayList<String>()
         this.userComments = ArrayList<String>()
+
+         */
     }
 
     constructor(username: String, email: String, password: String, userImg: Int, bio: String){
@@ -36,12 +40,19 @@ class User {
         this.userImg = userImg
         this.bio = bio
 
+        /*
         this.bookmarks = ArrayList<String>()
         this.userPosts = ArrayList<String>()
         this.highlights = ArrayList<String>()
         this.upvotedPosts = ArrayList<String>()
         this.usersFollowed = ArrayList<String>()
         this.userComments = ArrayList<String>()
+
+         */
+    }
+
+    fun getUserId(): String{
+        return this.userId
     }
 
     fun getUsername(): String {
@@ -87,6 +98,59 @@ class User {
     fun getUserComments(): ArrayList<String>{
         return this.userComments
     }
+
+
+
+
+
+    fun setUserId(userId: String){
+        this.userId = userId
+    }
+
+    fun setUsername(username: String) {
+        this.username = username
+    }
+
+    fun setEmail(email: String) {
+        this.email = email
+    }
+
+    fun setPassword(password: String) {
+        this.password = password
+    }
+
+    fun setUserImg(userImg: Int) {
+        this.userImg = userImg
+    }
+
+    fun setBio(bio: String) {
+        this.bio = bio
+    }
+
+    fun setBookmarks(bookmarks: ArrayList<String>){
+        this.bookmarks = bookmarks
+    }
+
+    fun setUserPosts(userPosts: ArrayList<String>){
+        this.userPosts = userPosts
+    }
+
+    fun setHighlights(highlights: ArrayList<String>){
+        this.highlights = highlights
+    }
+
+    fun setUpvotedPosts(upvotedPosts: ArrayList<String>){
+        this.upvotedPosts = upvotedPosts
+    }
+
+    fun setUsersFollowed(usersFollowed: ArrayList<String>){
+        this.usersFollowed = usersFollowed
+    }
+
+    fun setUserComments(userComments: ArrayList<String>) {
+        this.userComments = userComments
+    }
+
 }
 
 
