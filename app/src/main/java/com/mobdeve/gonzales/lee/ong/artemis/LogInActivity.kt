147a8c41ee3a80ce014e70c8bd3ee5881c28ce18
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-01 12:03:57 +0800

    Fix keyboard panning for search results activities

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
@@ -201,7 +201,7 @@ class LogInActivity : AppCompatActivity() {
 
     private fun startTesting() {
         this.btnTest.setOnClickListener {
-            val i = Intent(this@LogInActivity, EditPasswordActivity::class.java)
+            val i = Intent(this@LogInActivity, SearchResultsUnregisteredActivity::class.java)
             startActivity(i)
         }
     }
