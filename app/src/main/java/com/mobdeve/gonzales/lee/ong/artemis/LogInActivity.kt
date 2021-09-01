e3cdd6665dde625a1d43f070e219b4bba9b012c4
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-01 10:07:50 +0800

    Remove edit email and password from edit profile layout

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
@@ -205,7 +205,7 @@ class LogInActivity : AppCompatActivity() {
 
     private fun startTesting() {
         this.btnTest.setOnClickListener {
-            val i = Intent(this@LogInActivity, BrowseFeedActivity::class.java)
+            val i = Intent(this@LogInActivity, EditProfileActivity::class.java)
             startActivity(i)
         }
     }
