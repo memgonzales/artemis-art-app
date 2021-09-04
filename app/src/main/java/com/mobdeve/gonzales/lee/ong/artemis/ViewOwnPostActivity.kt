af8fe1fcb479488326fdd87fe14a1c206420b0ec
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-05 02:08:41 +0800

    Add Post functionality to all pertinent activities

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/ViewOwnPostActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/ViewOwnPostActivity.kt
@@ -1,11 +1,10 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
-
 import android.graphics.drawable.BitmapDrawable
-import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -14,6 +13,10 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -29,6 +32,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.File
 
 class ViewOwnPostActivity : AppCompatActivity() {
     private lateinit var civItemViewOwnPostProfilePic: CircleImageView
@@ -65,6 +69,21 @@ class ViewOwnPostActivity : AppCompatActivity() {
     private lateinit var cmFacebook: CallbackManager
     private lateinit var sdFacebook: ShareDialog
 
+    /**
+     * Photo of the artwork for posting.
+     */
+    private lateinit var photoFile: File
+
+    /**
+     * Activity result launcher related to taking photos using the device camera.
+     */
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+
+    /**
+     * Activity result launcher related to choosing photos from the Gallery
+     */
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_own_post)
@@ -94,6 +113,58 @@ class ViewOwnPostActivity : AppCompatActivity() {
         initComponents()
         initBottom()
         addPost()
+
+        initGalleryLauncher(this@ViewOwnPostActivity)
+        initCameraLauncher(this@ViewOwnPostActivity)
+    }
+
+    /**
+     * Initializes the activity result launcher related to choosing photos from the Gallery.
+     *
+     * @packageContext context tied to this activity
+     */
+    private fun initGalleryLauncher(packageContext: Context) {
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = Intent(packageContext, PostArtworkActivity::class.java)
+                intent.putExtra(
+                    Keys.KEY_POST_ARTWORK.name,
+                    result.data?.data.toString()
+                )
+
+                intent.putExtra(
+                    Keys.KEY_POST_FROM.name,
+                    PostArtworkUtil.FROM_GALLERY
+                )
+
+                startActivity(intent)
+            }
+        }
+    }
+
+    /**
+     * Initializes the activity result launcher related to taking photos using the device camera
+     *
+     * @packageContext context tied to this activity
+     */
+    private fun initCameraLauncher(packageContext: Context) {
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = Intent(packageContext, PostArtworkActivity::class.java)
+                intent.putExtra(
+                    Keys.KEY_POST_ARTWORK.name,
+                    photoFile.absolutePath
+                )
+
+                intent.putExtra(
+                    Keys.KEY_POST_FROM.name,
+                    PostArtworkUtil.FROM_CAMERA
+                )
+
+                startActivity(intent)
+            }
+        }
     }
 
     private fun initComponents() {
@@ -300,25 +371,11 @@ class ViewOwnPostActivity : AppCompatActivity() {
             this.clDialogPostArtworkPhoto = btmAddPost.findViewById(R.id.cl_dialog_post_artwork_photo)!!
 
             clDialogPostArtworkGallery.setOnClickListener {
-                Toast.makeText(
-                    this@ViewOwnPostActivity,
-                    "Photo chosen from the gallery",
-                    Toast.LENGTH_SHORT
-                ).show()
-                btmAddPost.dismiss()
-                val intent = Intent(this@ViewOwnPostActivity, PostArtworkActivity::class.java)
-                startActivity(intent)
+                PostArtworkUtil.chooseFromGallery(this, galleryLauncher)
             }
 
             clDialogPostArtworkPhoto.setOnClickListener {
-                Toast.makeText(
-                    this@ViewOwnPostActivity,
-                    "Photo taken with the device camera",
-                    Toast.LENGTH_SHORT
-                ).show()
-                btmAddPost.dismiss()
-                val intent = Intent(this@ViewOwnPostActivity, PostArtworkActivity::class.java)
-                startActivity(intent)
+                photoFile = PostArtworkUtil.takeFromCamera(this, this@ViewOwnPostActivity, cameraLauncher)
             }
 
             btmAddPost.show()
@@ -362,6 +419,55 @@ class ViewOwnPostActivity : AppCompatActivity() {
             Toast.makeText(this@ViewOwnPostActivity, "Opening Facebook", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this@ViewOwnPostActivity, "Unable to share post", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * Callback for the result from requesting permissions.
+     *
+     * @param requestCode The request code passed in <code>
+     *     ActivityCompat.requestPermissions(android.app.Activity, String[], int)</code>
+     * @param permissions The requested permissions. Never null
+     * @param grantResults The grant results for the corresponding permissions which is either <code>
+     *     PackageManager.PERMISSION_GRANTED</code> or <code>PackageManager.PERMISSION_DENIED</code>.
+     *     Never null
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsResult(requestCode, grantResults, this@ViewOwnPostActivity, this)
+    }
+
+    /**
+     * Defines the behavior related to choosing a photo from the Gallery or taking a photo using
+     * the device camera based on the permissions granted by the user.
+     *
+     * @param requestCode the request code passed in <code>
+     *     ActivityCompat.requestPermissions(android.app.Activity, String[], int)</code>
+     * @param grantResults the grant results for the corresponding permissions which is either <code>
+     *     PackageManager.PERMISSION_GRANTED</code> or <code>PackageManager.PERMISSION_DENIED</code>.
+     *     Never null
+     * @param context context tied to this activity
+     * @param activity this activity
+     */
+    private fun permissionsResult(requestCode: Int, grantResults: IntArray, context: Context,
+                                  activity: Activity
+    ) {
+        when (requestCode) {
+            RequestCodes.REQUEST_CODE_POST_CAMERA.ordinal -> {
+                val temp: File? = PostArtworkUtil.permissionsResultCamera(
+                    grantResults, activity,
+                    context, cameraLauncher
+                )
+
+                if (temp != null) {
+                    photoFile = temp
+                }
+            }
+
+            RequestCodes.REQUEST_CODE_POST_GALLERY.ordinal -> {
+                PostArtworkUtil.permissionsResultGallery(grantResults, context, galleryLauncher)
+            }
         }
     }
 }
