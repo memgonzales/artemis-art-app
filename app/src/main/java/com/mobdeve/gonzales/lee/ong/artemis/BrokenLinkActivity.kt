66426745a3527c28426dee77516591774eea443b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-08 12:19:03 +0800

    Add layout for catching broken links

---
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrokenLinkActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.gonzales.lee.ong.artemis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrokenLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_broken_link)
+    }
+}
