62c9a4f4385c579fc08185752ff78b8239499210
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-08 23:05:31 +0800

    Add email launcher to Broken Link activity

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrokenLinkActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/BrokenLinkActivity.kt
@@ -1,11 +1,33 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class BrokenLinkActivity : AppCompatActivity() {
+    private lateinit var tvBrokenLinkEmail: TextView
+    private val emailAddress: String = "artemis.appmaster@gmail.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broken_link)
+
+        launchEmail()
+    }
+
+    private fun launchEmail() {
+        tvBrokenLinkEmail = findViewById(R.id.tv_broken_link_email)
+        tvBrokenLinkEmail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND);
+
+            emailIntent.type = "message/rfc822";
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailAddress));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "[User Report] Persistent Broken Link");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Place your username, and describe " +
+                    "what you were doing when this error occurred");
+
+            startActivity(Intent.createChooser(emailIntent, "Send a report"));
+        }
     }
 }
