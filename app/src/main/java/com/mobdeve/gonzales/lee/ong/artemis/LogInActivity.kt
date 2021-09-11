ea200a691eba3d9d6c346345dd8435c228d7f7d0
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-09-11 21:56:13 +0800

    Add documentaion for Log In activity

--- app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
+++ app/src/main/java/com/mobdeve/gonzales/lee/ong/artemis/LogInActivity.kt
@@ -1,9 +1,7 @@
 package com.mobdeve.gonzales.lee.ong.artemis
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
@@ -18,25 +16,73 @@ import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
+/**
+ * Class handling the functionalities related to logging in.
+ *
+ * @constructor Creates a class that handles the functionalities related to logging in.
+ */
 class LogInActivity : AppCompatActivity() {
-    private lateinit var btnSignUp: Button
-    private lateinit var btnLogIn: Button
+
     private lateinit var btnTest: Button
 
+    /**
+     * Button for creating a user account.
+     */
+    private lateinit var btnSignUp: Button
+
+    /**
+     * Button for logging in.
+     */
+    private lateinit var btnLogIn: Button
+
+    /**
+     * Input field for the username.
+     */
     private lateinit var tietUsername: TextInputEditText
+
+    /**
+     * Input field for the password.
+     */
     private lateinit var tietPassword: TextInputEditText
 
+    /**
+     * Text input layout for the username.
+     */
     private lateinit var tilUsername: TextInputLayout
+
+    /**
+     * Text input layout for the password.
+     */
     private lateinit var tilPassword: TextInputLayout
 
+    /**
+     * Clickable text view for entering the app as an unregistered user (that is, via a guest account).
+     */
     private lateinit var tvGuest: TextView
 
+    /**
+     * Progress bar to signal that data are being fetched from the database.
+     */
     private lateinit var pbLogin: ProgressBar
 
-    //Firebase
+    /**
+     * Starting point for Firebase authentication SDK.
+     */
     private lateinit var mAuth: FirebaseAuth
+
+    /**
+     * Starting point for all database-related operations.
+     */
     private lateinit var db: DatabaseReference
 
+    /**
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState  If the activity is being re-initialized after previously being
+     * shut down then this Bundle contains the data it most recently supplied in
+     * <code>onSaveInstanceState(Bundle)</code>. Note: Otherwise it is <code>null</code>.
+     * This value may be <code>null</code>.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
@@ -45,11 +91,17 @@ class LogInActivity : AppCompatActivity() {
         initComponents()
     }
 
+    /**
+     * Initializes the Firebase-related components.
+     */
     private fun initFirebase(){
         this.mAuth = Firebase.auth
         this.db = Firebase.database.reference
     }
 
+    /**
+     * Initializes the components of the activity.
+     */
     private fun initComponents() {
         this.pbLogin = findViewById(R.id.pb_log_in)
 
@@ -66,6 +118,9 @@ class LogInActivity : AppCompatActivity() {
         loginAsGuest()
     }
 
+    /**
+     * Redirects the user to the activity for signing up.
+     */
     private fun launchSignUp() {
         this.btnSignUp.setOnClickListener {
             val i = Intent(this@LogInActivity, SignUpActivity::class.java)
@@ -73,6 +128,11 @@ class LogInActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Redirects the user to the activity for browsing the feed, provided that they
+     * entered valid log-in credentials (namely, the username or email address
+     * and the password).
+     */
     private fun startBrowsing() {
         this.btnLogIn.setOnClickListener {
             this.tietUsername = findViewById(R.id.tiet_log_in_username)
@@ -98,6 +158,15 @@ class LogInActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Checks if the user entered both a username (or email address) and a password.
+     *
+     * @param username value entered in the input field for the username (or email address)
+     * @param password value entered in the input field for the password.
+     *
+     * @return <code>false</code> if the user entered both a username (or email address) and
+     * a password; <code>true</code>, otherwise
+     */
     private fun checkEmpty(username: String, password: String): Boolean{
         var hasEmpty = false
 
@@ -124,6 +193,14 @@ class LogInActivity : AppCompatActivity() {
         return hasEmpty
     }
 
+    /**
+     * Verifies the log-in credentials given the email address and password the user entered.
+     *
+     * This function uses the built-in email and password authentication method of Firebase.
+     *
+     * @param email email address that the user entered
+     * @param password password that the user entered
+     */
     private fun loginWithEmail(email: String, password: String){
         this.mAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -137,6 +214,12 @@ class LogInActivity : AppCompatActivity() {
             }
     }
 
+    /**
+     * Verifies the log-in credentials given the username and password the user entered.
+     *
+     * @param username username that the user entered
+     * @param password password that the user entered
+     */
     private fun loginWithUsername(username: String, password: String){
         val userDB = this.db.child(Keys.KEY_DB_USERS.name)
 
@@ -165,6 +248,10 @@ class LogInActivity : AppCompatActivity() {
             })
     }
 
+    /**
+     * Defines the behavior when the user is successfully logged in, that is, by entering valid
+     * credentials.
+     */
     private fun loginSuccessfully(){
         this.pbLogin.visibility = View.GONE
         Toast.makeText(this@LogInActivity, "Logged in successfully", Toast.LENGTH_SHORT).show()
@@ -173,11 +260,18 @@ class LogInActivity : AppCompatActivity() {
         finish()
     }
 
+    /**
+     * Defines the behavior when the user inputs invalid credentials.
+     */
     private fun loginFailed(){
         this.pbLogin.visibility = View.GONE
         Toast.makeText(this@LogInActivity, "Invalid credentials", Toast.LENGTH_SHORT).show()
     }
 
+    /**
+     * Defines the behavior when the user chooses to enter the app as an unregistered user
+     * (that is, via a guest account).
+     */
     private fun loginAsGuest(){
         this.tvGuest.setOnClickListener {
             this.pbLogin.visibility = View.VISIBLE
@@ -199,6 +293,9 @@ class LogInActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Called when the activity is becoming visible to the user.
+     */
     override fun onStart() {
         super.onStart()
 
